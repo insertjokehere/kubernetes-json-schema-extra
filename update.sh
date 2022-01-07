6025d8d07909ed041600f6870ca8d0e2cfedadfe
William Hughes <will@willhughes.name>
2022-01-07 13:11:03 +1300

    Name things correctly

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ ! -d master ]; then
-    mkdir master
-fi
+rm -r master
+mkdir master
 
 if [ ! -s master-strict ]; then
     ln -s master master-strict
@@ -11,20 +10,20 @@ fi
 pushd master > /dev/null
 
 for src in \
-    https://raw.githubusercontent.com/jetstack/cert-manager/master/deploy/crds/crd-certificates.yaml \
-    https://raw.githubusercontent.com/jetstack/cert-manager/master/deploy/crds/crd-clusterissuers.yaml \
+    https://raw.githubusercontent.com/jetstack/cert-manager/v1.5.4/deploy/crds/crd-certificates.yaml \
+    https://raw.githubusercontent.com/jetstack/cert-manager/v1.5.4/deploy/crds/crd-clusterissuers.yaml \
     https://raw.githubusercontent.com/openfaas/faas-netes/master/artifacts/crds/openfaas.com_functions.yaml; do
 
-    ../scripts/openapi2jsonschema.py $src
+    FILENAME_FORMAT=\{kind\}-\{group\}-\{version\} ../scripts/openapi2jsonschema.py $src
 done
 
 popd > /dev/null
 
-for link in v1.21.0; do
+for link in v1.21.0 v1.21.1 v1.21.2; do
     if [ ! -s $link ]; then
         ln -s master $link
     fi
-    if [ ! -s $link-strict ]; then
-        ln -s master ${link}-strict
+    if [ ! -s $link-standalone ]; then
+        ln -s master ${link}-standalone
     fi
 done
