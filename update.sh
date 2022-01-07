f454dc7e0abff825b6c130ffd31c8791e39e4b32
William Hughes <will@willhughes.name>
2022-01-07 13:22:12 +1300

    Fixes

--- update.sh
+++ update.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-rm -r master
-mkdir master
+rm -r schemas
+mkdir schemas
 
-if [ ! -s master-strict ]; then
-    ln -s master master-strict
-fi
-
-pushd master > /dev/null
+pushd schemas > /dev/null
 
 for src in \
     https://raw.githubusercontent.com/jetstack/cert-manager/v1.5.4/deploy/crds/crd-certificates.yaml \
@@ -20,10 +16,12 @@ done
 popd > /dev/null
 
 for link in v1.21.0 v1.21.1 v1.21.2; do
-    if [ ! -s $link ]; then
-        ln -s master $link
+    if [ -e $link ]; then
+        rm -rf $link
     fi
-    if [ ! -s $link-standalone ]; then
-        ln -s master ${link}-standalone
+    cp -r schemas $link
+    if [ -e $link-standalone ]; then
+        rm -rf $link-standalone
     fi
+    cp -r schemas $link-standalone
 done
